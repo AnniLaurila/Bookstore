38079ec81ab3e0d8c20ef769aa43ac942b4340a6
unknown <anninmaili@gmail.com>
2020-02-10 20:35:56 +0200

    lisätty tietokannasta hakutoiminnalisuus

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,5 +1,6 @@
 package com.example.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,16 +8,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
 
 
 @Controller
 
 public class BookController {
 
-	@RequestMapping(value="/index", method=RequestMethod.GET)
-	@ResponseBody
-    public String books(Model model) {
-        model.addAttribute("book", new Book());
-        return "book";
+	@Autowired
+	private BookRepository repository;
+	
+	@RequestMapping(value= {"/", "/index", "booklist"}, method=RequestMethod.GET)
+    public String booklist(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
