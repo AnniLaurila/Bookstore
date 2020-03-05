37e0d620311f12b9575534a2adbc0e698d842cf5
Laurila Anni <bgk287@myy.haaga-helia.fi>
2020-03-05 15:08:00 +0200

    rest-rajapinta

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package com.example.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
@@ -27,6 +31,16 @@ public class BookController {
         return "booklist";
     }
 	
+	@RequestMapping(value= {"restbooklist"}, method=RequestMethod.GET)
+    public @ResponseBody List<Book> restBookList() {
+		return (List<Book>) repository.findAll();
+    }
+	
+	@RequestMapping(value= {"/restbook/{id}"}, method=RequestMethod.GET)
+    public @ResponseBody Optional<Book> restBook(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
+    }
+	
 	@RequestMapping(value="/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
@@ -43,6 +57,7 @@ public class BookController {
 	@RequestMapping(value="/edit/{id}")
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", repository.findById(bookId));
+		model.addAttribute("categorys", cRepository.findAll());
 		return "editbook";
 	}
 	
